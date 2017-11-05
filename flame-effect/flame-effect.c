08a22c7133a4921b53c9ab147d3aa3ab84ec0ff1
Stephen Clibbery <sdclibbery@googlemail.com>
2017-11-05 10:28:19 +0000

    animatoin; but the true colour output is not working (on git bash at least)

--- flame-effect/flame-effect.c
+++ flame-effect/flame-effect.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
+
+/*
+ToDO
+- Not working: its printing to nearest colour not true color :-(
+*/
 
 static void printColouredBlock (unsigned char r, unsigned char g, unsigned char b) {
-  printf("\x1b[38;2;%d;%d;%dm\xdb\xdb\x1b[0m", r, g, b);
+  printf("\x1b[38;2;%d;%d;%dm\u2588\u2588\x1b[0m", r, g, b);
 }
 
 static void setColor (unsigned char r, unsigned char g, unsigned char b) {
@@ -11,9 +17,11 @@ static void setColor (unsigned char r, unsigned char g, unsigned char b) {
 
 int main () {
   printColouredBlock(0,0,0);
-  setColor(2, 255, 100);
-  setColor(255, 255, 100);
-  setColor(2, 2, 100);
-  setColor(250, 2, 1);
+
+  for (unsigned char i=0; i<255; i++) {
+    setColor(i,i,i);
+    usleep(50000);
+  }
+
   return 0;
 }
