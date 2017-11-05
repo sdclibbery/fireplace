1b8d2bc77c705574e08296e5c352ae3c024a68ac
Stephen Clibbery <sdclibbery@googlemail.com>
2017-11-05 08:48:38 +0000

    define intended interface as both documentation and plan

---
+++ flame-effect/flame.h
@@ -0,0 +1,7 @@
+/*
+Flame colour effect. Reference: https://en.wikipedia.org/wiki/Flame
+Suggested interface:
+  rgb flameColour (blackbodyIntensity, radicalIntensity)
+blackbodyIntensity is the intensity of the (orange/yellow/white) blackbody component of flame colour
+radicalIntensity is the intensity of the (blue) component from spectral emission of burning free radicals
+*/
