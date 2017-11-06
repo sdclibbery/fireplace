d3a6d92b25297c6d9ee6fbc61a6b704d77d2051e
Stephen Clibbery <sdclibbery@googlemail.com>
2017-11-06 13:54:27 +0000

    simplify

--- flame-effect/blackbody.c
+++ flame-effect/blackbody.c
@@ -2,14 +2,13 @@
 
 #include "rgb.h"
 
-// blackbody radiation colours from http://www.tannerhelland.com/4435/convert-temperature-rgb-algorithm-code/
+// blackbody radiation colours from http://www.tannerhelland.com/4435/convert-hectokelvin-rgb-algorithm-code/
 
 static unsigned char red (unsigned int kelvin) {
-  float temperature = kelvin / 100.0f;
-  if (temperature <= 66.0f) {
+  if (kelvin <= 6600.0f) {
       return 255;
   } else {
-      float r = temperature - 60.0f;
+      float r = kelvin/100.0f - 60.0f;
       r = 329.698727446f * powf(r, -0.1332047592f);
       if (r < 0.0f) return 0;
       if (r > 255.0f) return 255;
@@ -18,15 +17,15 @@ static unsigned char red (unsigned int kelvin) {
 }
 
 static unsigned char green (unsigned int kelvin) {
-  float temperature = kelvin / 100.0f;
-  if (temperature <= 66.0f) {
-      float g = temperature;
+  float hectokelvin = kelvin / 100.0f;
+  if (hectokelvin <= 66.0f) {
+      float g = hectokelvin;
       g = 99.4708025861f * logf(g) - 161.1195681661f;
       if (g < 0) return 0;
       if (g > 255) return 255;
       return g;
   } else {
-      float g = temperature - 60.0f;
+      float g = hectokelvin - 60.0f;
       g = 288.1221695283f * powf(g, -0.0755148492f);
       if (g < 0) return 0;
       if (g > 255) return 255;
@@ -35,14 +34,14 @@ static unsigned char green (unsigned int kelvin) {
 }
 
 static unsigned char blue (unsigned int kelvin) {
-  float temperature = kelvin / 100.0f;
-  if (temperature >= 66.0f) {
+  float hectokelvin = kelvin / 100.0f;
+  if (hectokelvin >= 66.0f) {
       return 255;
   } else {
-      if (temperature <= 19.0f) {
+      if (hectokelvin <= 19.0f) {
           return 0;
       } else {
-          float b = temperature - 10.0f;
+          float b = hectokelvin - 10.0f;
           b = 138.5177312231f * logf(b) - 305.0447927307f;
           if (b < 0) b = 0;
           if (b > 255) b = 255;
