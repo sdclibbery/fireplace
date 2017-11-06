892ce63c131e5264695dd07e14a33fe7e5b39fad
Stephen Clibbery <sdclibbery@googlemail.com>
2017-11-06 14:00:45 +0000

    extract clamp helper

--- flame-effect/blackbody.c
+++ flame-effect/blackbody.c
@@ -4,15 +4,17 @@
 
 // blackbody radiation colours from http://www.tannerhelland.com/4435/convert-hectokelvin-rgb-algorithm-code/
 
+static unsigned char clamp (float x) {
+    return x < 0.0f ? 0 : (x > 255.0f ? 255 : x);
+}
+
 static unsigned char red (unsigned int kelvin) {
   if (kelvin <= 6600.0f) {
       return 255;
   } else {
       float r = kelvin/100.0f - 60.0f;
       r = 329.698727446f * powf(r, -0.1332047592f);
-      if (r < 0.0f) return 0;
-      if (r > 255.0f) return 255;
-      return (unsigned char) r;
+      return clamp(r);
   }
 }
 
@@ -20,15 +22,11 @@ static unsigned char green (unsigned int kelvin) {
   if (kelvin <= 6600.0f) {
       float g = kelvin / 100.0f;
       g = 99.4708025861f * logf(g) - 161.1195681661f;
-      if (g < 0) return 0;
-      if (g > 255) return 255;
-      return g;
+      return clamp(g);
   } else {
       float g = kelvin/100.0f - 60.0f;
       g = 288.1221695283f * powf(g, -0.0755148492f);
-      if (g < 0) return 0;
-      if (g > 255) return 255;
-      return g;
+      return clamp(g);
   }
 }
 
@@ -41,9 +39,7 @@ static unsigned char blue (unsigned int kelvin) {
       } else {
           float b = kelvin/100.0f - 10.0f;
           b = 138.5177312231f * logf(b) - 305.0447927307f;
-          if (b < 0) b = 0;
-          if (b > 255) b = 255;
-          return b;
+          return clamp(b);
       }
   }
 }
