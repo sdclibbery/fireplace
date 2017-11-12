7280e451174305197c4e869bce4be0b697004d11
Stephen Clibbery <sdclibbery@googlemail.com>
2017-11-12 19:11:17 +0000

    inline fixed point functions

--- flame-effect/build-and-run.sh
+++ flame-effect/build-and-run.sh
@@ -2,5 +2,5 @@
 # Build as a local executable to allow testing the flame effect
 # Run in a git prompt on windows
 
-gcc flametest.cpp flamecolour.cpp rgb.cpp flicker.cpp fixedpoint.cpp -lm -o flametest.exe
+gcc flametest.cpp flamecolour.cpp rgb.cpp flicker.cpp -lm -o flametest.exe
 ./flametest.exe
--- flame-effect/fixedpoint.h
+++ flame-effect/fixedpoint.h
@@ -1,2 +1,23 @@
-unsigned char fp8Mul (unsigned char l, unsigned char r);
-unsigned char fp8Cos (unsigned char x);
+static inline unsigned char fp8Mul (unsigned char l, unsigned char r) {
+  unsigned long out = (unsigned long)l * (unsigned long)r;
+  return (unsigned char)(out/255);
+}
+
+static inline unsigned char cosQ1 (unsigned char x) {
+  return 255 - ((unsigned long)x*x)/32;
+}
+
+static inline unsigned char fp8Cos (unsigned char x) {
+  unsigned char quadrant = x/64;
+  unsigned char v = x % 64;
+  switch (quadrant) {
+    case 0:
+      return cosQ1(v);
+    case 1:
+      return 255-cosQ1(63-v);
+    case 2:
+      return 255-cosQ1(v);
+    default:
+      return cosQ1(63-v);
+  }
+}
