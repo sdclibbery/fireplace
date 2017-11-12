58b783fb5b76932b71a9d4557671ec37ebb2916b
Stephen Clibbery <sdclibbery@googlemail.com>
2017-11-12 19:19:43 +0000

    inline rgb functions

--- flame-effect/rgb.h
+++ flame-effect/rgb.h
@@ -1,9 +1,16 @@
-typedef struct Rgb {
-  unsigned char r;
-  unsigned char g;
-  unsigned char b;
-} Rgb;
+#include "rgb.h"
+#include "fixedpoint.h"
 
-Rgb rgbScale (Rgb c, unsigned char scale);
-Rgb rgbAdd (Rgb l, Rgb r);
-Rgb rgbLerp (Rgb lo, Rgb hi, unsigned char x);
+static inline Rgb rgbScale (Rgb c, unsigned char s) {
+  Rgb out = { fp8Mul(c.r, s), fp8Mul(c.g, s), fp8Mul(c.b, s) };
+  return out;
+}
+
+static inline rgb rgbAdd (Rgb l, Rgb r) {
+  Rgb out = { l.r+r.r, l.g+r.g, l.b+r.b };
+  return out;
+}
+
+static inline Rgb rgbLerp (Rgb lo, Rgb hi, unsigned char x) {
+  return rgbAdd(rgbScale(hi, x), rgbScale(lo, 255-x));
+}
