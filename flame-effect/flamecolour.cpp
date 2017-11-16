dc99f95056a6213192378bd60c6e48bfa6e95c0a
Stephen Clibbery <sdclibbery@googlemail.com>
2017-11-16 00:03:19 +0000

    tweak flame colour

--- flame-effect/flamecolour.cpp
+++ flame-effect/flamecolour.cpp
@@ -2,9 +2,12 @@
 #include "fixedpoint.h"
 #include "rgb.h"
 
-static Rgb cool = { 255, 0, 0 };
-static Rgb hot = { 240, 112, 4 };
-
 Rgb flamecolour (unsigned char intensity) {
-  return rgbLerp(cool, hot, intensity);
+  unsigned int sqr = fp8Mul(intensity, intensity);
+  Rgb rgb = {
+    255,
+    fp8Mul(sqr, 160),
+    48 - fp8Mul(intensity, 48)
+  };
+  return rgb;
 }
