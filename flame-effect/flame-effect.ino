8c205527d8a99878760e6ed0873a46d228418ded
Stephen Clibbery <sdclibbery@googlemail.com>
2017-11-20 00:38:22 +0000

    web app controls presets

--- flame-effect/flame-effect.ino
+++ flame-effect/flame-effect.ino
@@ -20,14 +20,16 @@ static Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NE
 static const char* mDnsHostname = "fireplace";
 static ESP8266WebServer server(80);
 
+typedef Rgb (*FlameFunction)(unsigned char);
+
 static unsigned long frameIntervalMs = 10;
 static unsigned char brightness = 255;
+static FlameFunction flameColour = &woodFlame;
 static unsigned int flickerSpeed = 20;
 static unsigned long timeMsOffsets[NUMPIXELS];
 
 /*
 ToDo:
-wifi manager
 web app
 OTA updates?
 */
@@ -36,6 +38,7 @@ OTA updates?
 void setup() {
   Serial.begin(115200);
   Serial.println("Start up...");
+  
   WiFiManager wifiManager;
   while (!wifiManager.autoConnect("FIREPLACE-SETUP")) {
     Serial.println("WiFi start up failed.");
@@ -46,14 +49,32 @@ void setup() {
   MDNS.addService("http", "tcp", 80);
   MDNS.begin(mDnsHostname, WiFi.localIP());
   Serial.println("mDNS started");
+  
   server.on("/", [](){
-    server.send(200, "text/plain", "Hello World from fireplace!");
+    String content = "";
+    content += "<style>";
+    content += "body{font-family:helvetica,sans-serif;}";
+    content += "a{margin:8px;padding:4px;border:1px solid #aaa;background-color:#eee;color:#111;border-radius:4px;} a:hover{background-color:#ddd;}";
+    content += "</style>";
+    content += "<h1>Fireplace Control</h1>";
+    content += "<h3>Preset Effects</h3>";
+    Serial.println(String("Preset: ")+server.arg("preset"));
+    content += "<p><a href=\"/?preset=woodFlame\">Wood Flame</a></p>";
+    if (server.arg("preset") == "woodFlame") { flameColour = &woodFlame; }
+    content += "<p><a href=\"/?preset=gasFlame\">Gas Flame</a></p>";
+    if (server.arg("preset") == "gasFlame") { flameColour = &gasFlame; }
+    content += "<p><a href=\"/?preset=halloweenFlame\">Halloween</a></p>";
+    if (server.arg("preset") == "halloweenFlame") { flameColour = &halloweenFlame; }
+    content += "<p><a href=\"/?preset=rainbowFlame\">Gas Flame</a></p>";
+    if (server.arg("preset") == "rainbowFlame") { flameColour = &rainbowFlame; }
+    server.send(200, "text/html", content);
   });
   server.onNotFound([](){
     server.send(404, "text/plain", "404 not found");
   });
   server.begin();
   Serial.println("Server started");
+  
   for(int i=0;i<NUMPIXELS;i++){
     timeMsOffsets[i] = random(100000);
   }  
@@ -65,7 +86,7 @@ void loop() {
   for(int i=0;i<NUMPIXELS;i++){
     unsigned long pixelTimeMs = timeMs + timeMsOffsets[i];
     unsigned char intensity = flicker(flickerSpeed, pixelTimeMs);
-    Rgb rgb = rgbScale(woodFlame(intensity), brightness);
+    Rgb rgb = rgbScale(flameColour(intensity), brightness);
 //    pixels.setPixelColor(i, pixels.Color(rgb.r, rgb.g, rgb.b));
     pixels.setPixelColor(i, pixels.Color(128,0,90));
   }
